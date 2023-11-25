e662970ad57b89d3f75bee6f09a8b9b8b5318a75
sanketsakpal <sakpalsanket966@gmail.com>
2023-11-25 12:43:28 +0530

    [fix] - dec order code fix

--- problems/bin/dessending_order.dart
+++ problems/bin/dessending_order.dart
@@ -23,6 +23,7 @@ bubbleSort(List<int> array) {
   int lengthOfArray = array.length;
   for (int i = 0; i < lengthOfArray - 1; i++) {
     for (int j = 0; j < lengthOfArray - i - 1; j++) {
+      print("${array[j]} ${array[j + 1]}");
       if (array[j] < array[j + 1]) {
         // Swapping using temporary variable
         int temp = array[j];
@@ -31,5 +32,6 @@ bubbleSort(List<int> array) {
       }
     }
   }
-  return (array);
+  return array;
 }
+// 0 
