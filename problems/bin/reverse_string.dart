4b0dc24d85faf5d81798316ed7ca516b28293782
sanketsakpal <sakpalsanket966@gmail.com>
2023-12-18 16:58:50 +0530

    [wip] - solving revers string problem in dart

---
+++ problems/bin/reverse_string.dart
@@ -0,0 +1,13 @@
+void main(List<String> args) {
+  String test = "hellow";
+  String reverse = '';
+  for (var i = 0; i < test.length; i++) {
+    test[i].split('');
+    reverse = reverse + test[i];
+  }
+  print(test.split('').reversed.join());
+}
+
+String reverseString(String reverse) {
+  return reverse;
+}
