2a0a4a782c29de6d3127b1d4fd0c0e65b666f39d
sanketsakpal <sakpalsanket966@gmail.com>
2023-12-21 11:32:27 +0530

    [feature] - reverse String solution

--- problems/bin/reverse_string.dart
+++ problems/bin/reverse_string.dart
@@ -1,13 +1,40 @@
 void main(List<String> args) {
-  String test = "hellow";
-  String reverse = '';
-  for (var i = 0; i < test.length; i++) {
-    test[i].split('');
-    reverse = reverse + test[i];
-  }
-  print(test.split('').reversed.join());
+  String test = "hello";
+  // String reverse = reverseString(test);
+  // print(reverse);
+  reverseString2(test);
+  String originalString = "Hello, World!";
+  String reversed = reverseString(originalString);
+
+  print('Original String: $originalString');
+  print('Reversed String: $reversed');
 }
 
-String reverseString(String reverse) {
-  return reverse;
+String reverseString(String input) {
+  String reversed = '';
+  for (int i = input.length - 1; i >= 0; i--) {
+    reversed = reversed + input[i];
+  }
+  return reversed;
+}
+
+String reverseString2(String input) {
+  // Convert the string to a list of characters
+  List<String> characters = input.split('');
+
+  // Get the length of the string
+  int length = characters.length;
+
+  // Use a for loop to reverse the list in-place
+  for (int i = 0; i < length ~/ 2; i++) {
+    // Swap characters at positions i and length - 1 - i
+    String temp = characters[i];
+    characters[i] = characters[length - 1 - i];
+    characters[length - 1 - i] = temp;
+  }
+
+  // Join the reversed characters to form the reversed string
+  String reversedString = characters.join('');
+
+  return reversedString;
 }
